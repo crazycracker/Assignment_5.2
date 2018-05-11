e4699456e428ddfa8bc4b5df288290bd5dd8d7ae
vinay raj <vinayraj33@live.com>
2018-05-11 19:28:40 +0530

    first commit

---
+++ problem_statement_1.py
@@ -0,0 +1,8 @@
+subjects = ['Americans','Indians']
+verbs = ['play', 'watch']
+objects = ['Baseball', 'cricket']
+
+for sub in subjects:
+    for verb in verbs:
+        for obj in objects:
+            print(sub + ' ' + verb + ' ' + obj)
